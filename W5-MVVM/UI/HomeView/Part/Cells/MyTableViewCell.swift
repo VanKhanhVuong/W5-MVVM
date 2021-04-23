e079cc01f4381d4c27caf69da14a2f78ee24c538
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-04-23 10:13:00 +0700

    feat: Commit View, ViewModel, ViewController of TableViewCell

---
+++ W5-MVVM/UI/HomeView/Part/Cells/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  W5-MVVM
+//
+//  Created by Văn Khánh Vương on 22/04/2021.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    func bildData(viewModel : MyTableViewCellsModels) {
+        self.nameLabel.text = "Name: " + viewModel.name
+        self.genderLabel.text = "Gender: " + viewModel.gender
+        self.ageLabel.text = "Age: \(viewModel.age)"
+    }
+}
---
+++ W5-MVVM/UI/HomeView/Part/Cells/MyTableViewCellsModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyTableViewCellsModel.swift
+//  W5-MVVM
+//
+//  Created by Văn Khánh Vương on 22/04/2021.
+//
+
+import Foundation
+
+struct MyTableViewCellsModels {
+    let name: String
+    let age: Int
+    let gender: String
+    
+}
